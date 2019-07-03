0639d412ae08815cb8cf570c521768a081f32205
Miguel Alonso <malbonm07@gmail.com>
2019-07-02 19:17:11 -0500

    added name to swiper

--- src/components/track.vue
+++ src/components/track.vue
@@ -15,15 +15,14 @@
           </div>
           <div v-show="false" >
             <audio :src="trackObject.preview_url" ref="trackAudio" controls>
-
             </audio>
           </div>
         </q-img>
-        <q-item-section top class="flex flex-center q-pt-sm" v-if="folder">
+        <q-item-section top class="flex flex-center q-pt-sm">
           <q-item-label lines="1">
             <span class="text-subtitle2 text-capitalize text-white">{{trackObject.name}}</span>
           </q-item-label>
-          <q-item-label lines="1" class="q-mt-xs text-body2 text-weight-bold text-grey-9 text-capitalize">
+          <q-item-label lines="1" class="q-mt-xs text-body2 text-weight-bold text-grey-9 text-capitalize" v-if="folder">
             <span class="text-caption text-weight-light text-grey">Add to Folder</span>
             <q-btn flat round color="white" 
                    icon="folder_open"
